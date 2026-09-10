23e0219a915645d8fec7a4b3cd56d714f36649c5
Peter Kaufman <PeterJamesKaufman@gmail.com>
2026-09-09 20:02:38 -0400

    cleanup replaces for the docs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -26,20 +26,18 @@ const mockedPlugins = [replace({
     // update usage of moment from obsidian to the node implementation of moment we have
     'import {moment} from \'obsidian\';': 'import moment from \'moment\';',
     // remove the use of obsidian in the options to allow for docs.js to run
-    'import {App, ExtraButtonComponent, normalizePath, Setting, TFile, ToggleComponent} from \'obsidian\';': '',
-    'import type {SettingDefinition, SettingDefinitionGroup, SettingDefinitionItem, SettingDefinitionPage} from \'obsidian\';': '',
-    // remove the use of obsidian in settings helper to allow for docs.js to run
-    'import {App, MarkdownRenderer} from \'obsidian\';': '',
+    'import {App, ExtraButtonComponent, normalizePath, TFile, ToggleComponent} from \'obsidian\';': '',
+    'import type {SettingDefinition, SettingDefinitionItem, SettingDefinitionList} from \'obsidian\';': '',
     // remove the use of obsidian in the auto-correct files picker to allow for docs.js to run
     'import {Setting, App, TFile, normalizePath, ExtraButtonComponent} from \'obsidian\';': '',
     // remove the use of obsidian in add custom row to allow for docs.js to run
     'import {App, Setting} from \'obsidian\';': '',
-    // remove the use of obsidian in suggest to allow for docs.js to run
-    'import {App, ISuggestOwner, Scope} from \'obsidian\';': '',
     // remove the use of obsidian in md file suggester to allow for docs.js to run
     'import {AbstractInputSuggest, App, TFile} from \'obsidian\';': '',
     // remove the use of obsidian in parse results modal to allow for docs.js to run
     'import {Modal, App} from \'obsidian\';': 'class Modal {}',
+    // remove the use of obsidian in modals to allow for docs.js to run
+    'import {Notice, Modal, App} from \'obsidian\';': 'class Modal {}',
     // remove the use of app from a couple of settings for docs.js to run
     'import {App} from \'obsidian\';': '',
     // remove the use of sanitizeHTMLToDom from the ui helpers for docs.js to run
